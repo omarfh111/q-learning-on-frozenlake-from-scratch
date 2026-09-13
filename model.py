7d637baf09541953cf4bda410a4c5629822d4d3e
Omar Fkih Hassen <163393752+omarfh111@users.noreply.github.com>
2026-09-13 15:58:26 +0100

    Step 5: should_explore

--- model.py
+++ model.py
@@ -38,8 +38,11 @@ def sample_random_action(action_space):
     # TODO: draw a uniformly random action from the given Gymnasium action space
     return int(action_space.sample())
 
-# Step 5 - should_explore (not yet solved)
-# TODO: implement
+# Step 5 - should_explore
+def should_explore(epsilon, rng):
+    """Return True with probability epsilon using the provided numpy Generator."""
+    # TODO: draw a uniform sample from rng and compare it to epsilon
+    return rng.random() < epsilon
 
 # Step 6 - epsilon_greedy_action (not yet solved)
 # TODO: implement
